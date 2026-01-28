89b0074292549214a89f8a13e4799f29a3d8cd32
Aman <aman.p@simformsolutions.com>
2026-01-28 23:35:27 +0530

    chore: setup nuxt 3 with tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline text-center mt-10">
+      Hello from Tailwind CSS v4!
+    </h1>
   </div>
 </template>
